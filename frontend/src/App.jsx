7891e9252a2ad8237cafdea93ee9eaa135856530
vandan0101 <patelvandan010107@gmail.com>
2026-06-11 10:43:16 +0530

    frontend login and signup pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Start from './pages/Start.jsx'
+import UserLogin from './pages/UserLogin.jsx'
+import UserSignup from './pages/UserSignup.jsx'
+import Captainlogin from './pages/Captainlogin.jsx'
+import CaptainSignup from './pages/CaptainSignup.jsx'
+
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/login' element={<UserLogin />} />
+       
+
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App
