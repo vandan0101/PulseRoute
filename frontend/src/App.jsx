93a8a83cdbe6de4f2a1c65d69761842a79a2d14a
KrishKyada <krishkyada77@gmail.com>
2026-06-12 15:19:00 +0530

    Add: implement Home, UserLogout, and UserProtectWrapper components with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Start from './pages/Start.jsx'
-import UserLogin from './pages/UserLogin.jsx'
-import UserSignup from './pages/UserSignup.jsx'
-import Captainlogin from './pages/Captainlogin.jsx'
-import CaptainSignup from './pages/CaptainSignup.jsx'
+import Start from './pages/Start'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/Captainlogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Home from './pages/Home'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
 
 
 
@@ -20,6 +23,17 @@ const App = () => {
         <Route path='/signup' element={<UserSignup />} />
         <Route path='/captain-login' element={<Captainlogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/home'
+          element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          } />
+          <Route path='/user/logout'
+          element={<UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+          } />
         
       </Routes>
     </div>
