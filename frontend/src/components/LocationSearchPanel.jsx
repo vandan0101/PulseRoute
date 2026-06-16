b4790fdf0b6bafc1b64677d2d6a5bcb0d6480c10
vandan0101 <patelvandan010107@gmail.com>
2026-06-16 15:12:41 +0530

    add animation in home page

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const LocationSearchPanel = () => {
   return (
     <div>
-        <div>LocationSearchPanel</div>
+      {/* this is just a sample data */}
+
+      <div className='flex gap-4 items-center my-2 justify-start'>
+         <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'><i className="ri-map-pin-fill"></i></h2>
+
+        <h4 className='font-medium'>
+          24B, Near Kapoor's cafe, Sheryians Coding School
+        </h4>
+      </div>
     </div>
   )
 }
