c8003023d4d8e4dd3cd52fd0d8cba0d7fb242f34
vandan0101 <patelvandan010107@gmail.com>
2026-06-30 15:18:17 +0530

    maps and ride routes

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const connectToDb = require('./DB/db');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes')
 const captainRoutes = require('./routes/captain.routes')
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
@@ -22,5 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
